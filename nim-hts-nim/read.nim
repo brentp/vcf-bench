8d5c746720e917915bd4192ec1ef2c24fa0f9ce4
Brent Pedersen <bpederse@gmail.com>
2021-11-16 21:46:12 +0100

    simplify nim

--- nim-hts-nim/read.nim
+++ nim-hts-nim/read.nim
@@ -1,26 +1,22 @@
 import hts/vcf
 import os
+import math
 
 proc main() =
 
-  var f = paramStr(1)
-
   var ivcf:VCF
-  if not ivcf.open(f):
+  if not ivcf.open(paramStr(1)):
     quit "couldn't open input vcf"
-  #ivcf.set_samples(@["^"])
 
+  #ivcf.set_samples(@["^"])
   var an = newSeq[int32](1)
   var li = newSeqOfCap[int64](65536)
   for v in ivcf:
-
     if v.info.get("AN", an) != Status.OK:
       continue
     li.add(an[0])
 
-  var s = 0'i64
-  for v in li: s += v
-  echo s.float64 / li.len.float64
+  echo sum(li).float64 / li.len.float64
 
 
 when isMainModule:
