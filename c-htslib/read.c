f0afc7ede1161b8e47aaeee7ea3a1aec0bbc5c4b
Brent Pedersen <bpederse@gmail.com>
2021-11-17 19:39:05 +0100

    make c impl match what other are doing

--- c-htslib/read.c
+++ c-htslib/read.c
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include "htslib/vcf.h"
 #include "htslib/hts.h"
+#include <stdint.h>
+#include "kvec.h"
 
 int main(int argc, char *argv[]) {
 
   char *fname = argv[1];
+
+  kvec_t(int32_t) array;
+  kv_init(array);
 
   htsFile *fp = hts_open(fname, "r");
   bcf_hdr_t *hdr = bcf_hdr_read(fp);
@@ -13,20 +18,22 @@ int main(int argc, char *argv[]) {
   int *values = 0;
   int count = 0;
 
-  double s = 0;
-  int n = 0;
 
   while (bcf_read(fp, hdr, line) == 0) {
     if (bcf_get_info_int32(hdr, line, "AN", &values, &count) > 0) {
-      s += *values;
-      n += 1;
+      kv_push(int32_t, array, *values);
     }
+  }
+  double s = 0;
+  for(int i=0; i<kv_size(array);i++) {
+    s += kv_A(array, i);
   }
 
   bcf_hdr_destroy(hdr);
   hts_close(fp);
   bcf_destroy(line);
   free(values);
+  kv_destroy(array);
 
-  fprintf(stderr, "%.2f\n", s / (double)n);
+  fprintf(stderr, "%.2f\n", s / (double)kv_size(array));
 }
