90ec7a0cd7cdf149806f771228a8f60f88279d4e
Brent Pedersen <bpederse@gmail.com>
2021-11-18 13:43:55 +0100

    simplify zig

--- zig-hts-zig/src/main.zig
+++ zig-hts-zig/src/main.zig
@@ -3,19 +3,18 @@ const vcf = @import("./vcf.zig");
 const stdout = std.io.getStdOut().writer();
 
 pub fn main() anyerror!void {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = std.heap.c_allocator;
     var args = std.process.args();
     _ = args.skip();
-    var fname = try args.next(&gpa.allocator).?;
-    defer _ = gpa.deinit();
-    defer gpa.allocator.free(fname);
+    var fname = try args.next(allocator).?;
+    defer allocator.free(fname);
 
     var ivcf = vcf.VCF.open(fname).?;
     defer ivcf.deinit();
 
     var fld: []const u8 = "AN";
-    var ans = std.ArrayList(i32).init(&gpa.allocator);
-    var values = std.ArrayList(i32).init(&gpa.allocator);
+    var ans = std.ArrayList(i32).init(allocator);
+    var values = std.ArrayList(i32).init(allocator);
     defer values.deinit();
     defer ans.deinit();
 
@@ -30,5 +29,5 @@ pub fn main() anyerror!void {
         s += @intToFloat(f64, value);
     }
 
-    try stdout.print("mean:{any}\n", .{s / @intToFloat(f64, values.items.len)});
+    try stdout.print("mean:{d:4.2}\n", .{s / @intToFloat(f64, values.items.len)});
 }
